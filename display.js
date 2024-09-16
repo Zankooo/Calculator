f678b863f801c4dd9212da9b9cbdbe0eb1567d82
Zankooo <stankovic.zan@gmail.com>
2024-09-16 22:49:42 +0200

    backspace dela ampak treba dodat se pogoje

--- display.js
+++ display.js
@@ -9,24 +9,43 @@ for (let button of buttons) {
 let izraz = ""
 
 function onclick(event){
-    
+    //kle je blo zafrkancije ker img ne more imet value in pol mu das ta getAttribute
     let value = event.target.value;
-    
-    console.log("vrednost kliknjena je " , value)
-    if (value === "C"){
-        display.innerHTML = ""
-        izraz = ""
+    let value_backspace = event.target.getAttribute('data-value');
+    let vrednost;
+    //in pol gledamo v temu ifu pogoje da nastavimo potem to vrednost ki 'gre naprej' potem
+    // ce je value undefined pomeni da smo kliknili ikonco, kar je okej, ce je pa value backspaceGumb pa pomeni da smo kliknili button = oboje pa skupaj tvori button
+    if ((value === undefined) || (value == "backspaceGumb")){
+        vrednost = "backspace"
+        console.log("DELA")
+    }
+    else{
+        vrednost = value
     }
     
-    else if (value === "=") {
+    if (vrednost === "backspace"){
+        if (izraz.length > 0){
+            izraz = izraz.slice(0, -1)
+        }
+        
+        
+    }
+    
+    else if (vrednost === "=") {
         izracunaj(izraz)
         return
     }
     else {
         izraz = izraz.concat(value)
     }
+
     display.innerHTML = izraz
     console.log("IZRAZ JE TOLE: " + izraz)
+    //resetiramo te vrednosti za novo sejo potem
+    vrednost = ""
+    value = ""
+    value_backspace = ""
+    
 }
 
 
